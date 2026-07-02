22c9f3e1714e219804bfe1048590e3eb1f538a44
neeraj gautam <neerajgautam@gmail.com>
2026-07-02 09:54:58 +0530

    add nothing in main file

--- core-java-practice/gcr-codebase/exception-handling/Class-work-Example/NumberFormatExceptionExample.java
+++
@@ -1,9 +0,0 @@
-public class NumberFormatExceptionExample {
-    public static void main(String[] args) {
-        try {
-            int number = Integer.parseInt("XYZ");
-        } catch (NumberFormatException e) {
-            System.out.println("NumberFormatException caught: " + e.getMessage());
-        }
-    }
-}
